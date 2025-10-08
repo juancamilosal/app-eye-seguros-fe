e4bd67c4bd1127e25ba3e7b717008b5328c6dfc8
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2025-10-08 16:49:40 -0500

    Mi primer commit

---
+++ src/app/layouts/private-layout/private-layout.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-private-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './private-layout.html'
+})
+export class PrivateLayout {
+
+}
