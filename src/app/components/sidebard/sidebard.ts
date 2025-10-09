88bd12ccf3e93e7fd51c8f70a1f9bb131914bc67
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2025-10-08 21:12:20 -0500

    Se crea el botón hamburguesa en versión mobile.

--- src/app/components/sidebard/sidebard.ts
+++ src/app/components/sidebard/sidebard.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, Router } from '@angular/router';
 
@@ -10,9 +10,13 @@ import { RouterLink, RouterLinkActive, Router } from '@angular/router';
   styleUrls: ['./sidebard.css']
 })
 export class Sidebard {
+  @Output() select = new EventEmitter<void>();
   constructor(private router: Router) {}
 
   logout() {
     this.router.navigateByUrl('/login');
+  }
+  onSelect() {
+    this.select.emit();
   }
 }
