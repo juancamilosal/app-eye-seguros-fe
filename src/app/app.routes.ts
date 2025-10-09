0768282002ac94e3027a1ba8e8f155f45e6bc787
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2025-10-08 21:23:06 -0500

    Se ajusta imagen para el navegador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'login',
+    title: 'Euyenid & Edder | Login',
     loadComponent: () => import('./layouts/public-layout/pages/login/login').then(m => m.Login)
   },
   {
@@ -11,14 +12,17 @@ export const routes: Routes = [
     children: [
       {
         path: 'dashboard',
+        title: 'Euyenid & Edder | Dashboard',
         loadComponent: () => import('./layouts/private-layout/pages/dashboard/dashboard').then(m => m.Dashboard)
       },
       {
         path: 'clients',
+        title: 'Euyenid & Edder | Clientes',
         loadComponent: () => import('./layouts/private-layout/pages/clients/clients').then(m => m.Clients)
       },
       {
         path: 'gestion',
+        title: 'Euyenid & Edder | Gestión',
         loadComponent: () => import('./layouts/private-layout/pages/gestion/gestion').then(m => m.Gestion)
       }
     ]
