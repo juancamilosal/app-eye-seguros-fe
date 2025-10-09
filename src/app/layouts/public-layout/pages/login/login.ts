a83d88da29442ceec3b0962fcd50870564dd8deb
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2025-10-08 19:49:31 -0500

    Se crea modal para notificaciones

--- src/app/layouts/public-layout/pages/login/login.ts
+++ src/app/layouts/public-layout/pages/login/login.ts
@@ -1,12 +1,13 @@
 import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { NotificationModalComponent } from '../../../../components/notification-modal/notification-modal';
+import { NotificationData } from '../../../../core/models/NotificationData';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NotificationModalComponent],
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
@@ -15,12 +16,32 @@ export class Login {
   password = '';
   isSubmitting = false;
   currentYear = new Date().getFullYear();
+  isModalVisible = false;
+  notification: NotificationData | null = null;
 
   submit() {
     this.isSubmitting = true;
     setTimeout(() => {
       this.isSubmitting = false;
-      alert('Inicio de sesión simulado');
+      const ok = !!this.email && !!this.password && this.password.length >= 6;
+      this.isModalVisible = true;
+      if (ok) {
+        this.notification = {
+          type: 'success',
+          title: 'Inicio de sesión exitoso',
+          message: 'Has iniciado sesión correctamente.'
+        };
+      } else {
+        this.notification = {
+          type: 'error',
+          title: 'Error al iniciar sesión',
+          message: 'Verifica tu correo y contraseña.'
+        };
+      }
     }, 800);
+  }
+
+  onModalClosed() {
+    this.isModalVisible = false;
   }
 }
