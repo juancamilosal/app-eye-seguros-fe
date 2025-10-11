62b0edc75460bacb67b7ed0bdf2c158ff5d62fba
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2025-10-10 22:42:05 -0500

    Se crea la tabla de clientes

--- src/app/layouts/private-layout/pages/clientes/clientes.ts
+++ src/app/layouts/private-layout/pages/clientes/clientes.ts
@@ -8,5 +8,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './clientes.html'
 })
 export class Clientes {
-
+  clientes = [
+    {
+      nombre: 'María',
+      apellido: 'González',
+      fecha_nacimiento: new Date(1990, 4, 12),
+      direccion: 'Av. Principal 123, Caracas',
+      email: 'maria.gonzalez@example.com'
+    },
+    {
+      nombre: 'Juan',
+      apellido: 'Pérez',
+      fecha_nacimiento: new Date(1985, 10, 3),
+      direccion: 'Calle 45 #67-89, Bogotá',
+      email: 'juan.perez@example.com'
+    }
+  ];
 }
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+const apiUrl = 'http://directus-tckww440wkcsos0o0csk8kwk.92.113.149.220.sslip.io/';
+
+export const environment = {
+  production: false,
+
+  crearCliente: apiUrl + 'items/clientes',
+};
