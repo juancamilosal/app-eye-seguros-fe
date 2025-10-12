f25b84ef1f0520c681c12bded9a1c3a22bc5a8dd
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2025-10-11 19:46:12 -0500

    Se ajusta envioroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,6 @@ const apiUrl = 'http://directus-tckww440wkcsos0o0csk8kwk.92.113.149.220.sslip.io
 export const environment = {
   production: true,
   clientes: apiUrl + 'items/clientes',
-  crearCliente: apiUrl + 'items/clientes',
-  listarClientes: apiUrl + 'items/clientes',
   authLogin: apiUrl + 'auth/login',
   authMe: apiUrl + 'users/me',
 };
